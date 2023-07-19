72bfef9f722531b2f958b3892f883f7c6050e590
Sw_abah <ahmedswabah922@gmail.com>
2023-07-18 22:03:43 -0400

    home content changed

--- src/Pages/Homepage/Home.jsx
+++ src/Pages/Homepage/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
         <div className="text-4xl w-full h-full flex flex-col md:flex-row justify-center items-center gap-y-5 md:gap-y-0 lg:pb-20 font-extrabold text-start text-[#e8effd]">
         <div className='w-full md:w-1/2 flex flex-col items-center text-center md:text-start md:items-start text-[#0d1d39] justify-center gap-3 lg:gap-10'>
           <div className='w-auto drop-shadow-lg tracking-wide md:tracking-wider leading-snug uppercase text-5xl lg:text-6xl xl:text-7xl md:space-y-3'>
-              <h1>Discover the</h1>
-              <h1>Amazing World</h1>
-              <h1 className='bg-[#030a19] text-white w-auto px-1 md:p-1 md:px-3'>of Offers</h1>
+              <h1>open the</h1>
+              <h1> World of </h1>
+              <h1 className='bg-[#030a19] text-white w-auto px-1 md:p-1 md:px-3'>Offers</h1>
             </div>
             <p className='text-sm md:text-xl tracking-wide opacity-80'>Wide Range of Choices</p>
           </div>
